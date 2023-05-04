1be1d9919e43336d007888400ab23984afcb74de
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2023-05-04 20:15:03 +0200

    alter file_data.py and remove files_list_data.py

--- serialization_classes/file_data.py
+++ serialization_classes/file_data.py
@@ -1,18 +1,14 @@
 class FileData:
-    def __init__(self, file_name, chunk, chunk_hash, chunk_serial_num, end_of_file, experiment_name):
+    def __init__(self, file_name, data, data_hash, experiment_name):
         self.file_name = file_name
-        self.chunk = chunk
-        self.chunk_hash = chunk_hash
-        self.chunk_serial_num = chunk_serial_num
-        self.end_of_file = end_of_file
+        self.data = data
+        self.data_hash = data_hash
         self.experiment_name = experiment_name
 
     def to_dict(self):
         return {
             "file_name": self.file_name,
-            "chunk": self.chunk,
-            "chunk_hash": self.chunk_hash,
-            "chunk_serial_num": self.chunk_serial_num,
-            "end_of_file": self.end_of_file,
+            "data": self.data,
+            "data_hash": self.data_hash,
             "experiment_name": self.experiment_name
         }
